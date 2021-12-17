5601edf6cc70e50e68398579f0980434017409fb
kou2911 <pnonkiti@icloud.com>
2021-12-17 15:03:49 +0900

    controller設定

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -5,7 +5,16 @@ class DiariesController < ApplicationController
   end
 
   def new
-    
+    @diary = Diary.new
+  end
+
+  def create
+    @diary = Diary.new
+    if @diary.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
 
