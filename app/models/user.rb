e22fd8b068122ee6efc99ee6f075c0b27e1ed56e
kou2911 <pnonkiti@icloud.com>
2021-12-19 11:46:35 +0900

    アソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ApplicationRecord
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
     validates_format_of :password, with: PASSWORD_REGEX
   end
+  has_many :diaries
 end
