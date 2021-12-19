6210bfab4bc591eabdeb6e989bd430c13d369502
kou2911 <pnonkiti@icloud.com>
2021-12-19 20:40:06 +0900

    controller設定

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,4 +1,5 @@
 class DiariesController < ApplicationController
+  before_action :authenticate_user!, only: [:new,:create]
 
 
   def index
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+
 
   def show
     @nickname = current_user.nickname
