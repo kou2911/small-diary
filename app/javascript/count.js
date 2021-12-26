3c8b38615ec28c709f0a7a924a05ee278bb99ffa
kou2911 <pnonkiti@icloud.com>
2021-12-26 14:27:01 +0900

    文字数カウント

--- app/javascript/count.js
+++ app/javascript/count.js
@@ -1,6 +1,19 @@
 function count() {
-  const WordCount = document.getElementById()
-}
+  const content = document.getElementById("diary-content");
+  content.addEventListener("keyup",()=>{
+    const countNum = content.value.length;
+    const WordCount = document.getElementById("word-count");
+    WordCount.innerHTML =`${countNum}/15`;
+  });
+
+  const placeDiary =document.getElementById("diary-place");
+  placeDiary.addEventListener("keyup",()=>{
+    const numCount = placeDiary.value.length;
+    const PlaceCount = document.getElementById("place-count");
+    PlaceCount.innerHTML =`${numCount}/30`
+  });
+
+};
 
 
 
