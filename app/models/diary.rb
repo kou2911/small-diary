557b14451a5c27fc76d2808ac58525fa9e5d0536
kou2911 <pnonkiti@icloud.com>
2021-12-26 20:52:40 +0900

    diaryモデル　単体テストコード

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,13 +1,7 @@
 class Diary < ApplicationRecord
   has_one_attached :image
   belongs_to :user
-  validates :content, presence: true, unless: :was_attached?
-  validates :place,   presence: true
-  
-
-  def was_attached?
-    self.image.attached?
-  end
-
+  validates :content, presence: true, length:{maximum:15}
+  validates :place,   presence: true, length:{maximum:30}
 
 end
--- spec/models/diary_spec.rb
+++ spec/models/diary_spec.rb
@@ -1,5 +1,72 @@
 require 'rails_helper'
 
 RSpec.describe Diary, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @diary = FactoryBot.build(:diary)
+    end
+
+    context '新規投稿できる場合' do
+      it '全ての値が存在する' do
+        expect(@diary).to be_valid
+      end
+
+      it '画像がなくても投稿できる' do
+        @diary.image = nil
+        expect(@diary).to be_valid
+      end
+    end
+
+
+    context '新規投稿ができない場合' do
+      it 'contentが空だと投稿できない' do
+        @diary.content = ''
+        @diary.valid?
+        expect(@diary.errors.full_messages).to include("内容を入力してください")
+      end
+
+      it 'placeが空だと投稿できない' do
+        @diary.place = ''
+        @diary.valid?
+        expect(@diary.errors.full_messages).to include("場所を入力してください")
+        
+      end
+
+      it 'userが紐付いていないと保存できないこと' do
+        @diary.user = nil
+        @diary.valid?
+        expect(@diary.errors.full_messages).to include("Userを入力してください")
+        
+      end
+
+      it 'contentが15文字より多いと保存できないこと' do
+        @diary.content = "aaaaaaaaaaaaaaaaa"
+        @diary.valid?
+        expect(@diary.errors.full_messages).to include("内容は15文字以内で入力してください")
+      end
+
+      it 'placeが30文字より多いと保存できないこと' do
+        @diary.place = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+        @diary.valid?
+        expect(@diary.errors.full_messages).to include("場所は30文字以内で入力してください")
+      end
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  end
 end
