0f156c616fee8d5120a7865d22a2960620605556
kou2911 <pnonkiti@icloud.com>
2022-01-08 15:36:26 +0900

    修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,7 +109,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-    # herokuでメールを送信するための設定
     config.action_mailer.delivery_method = :smtp
     host = "https://small-diary2911.herokuapp.com/"
     config.action_mailer.default_url_options = { host: host, protocol: 'https' }
