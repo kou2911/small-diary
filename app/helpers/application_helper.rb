b3c4100533e4b02dbd46985f004a9c9127a5d5ea
kou2911 <pnonkiti@icloud.com>
2022-01-30 21:46:58 +0900

    OGP修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,27 @@
 module ApplicationHelper
-  def default_meta_tags
     def default_meta_tags
     {
-      site: 'Small-diary',
+      site: '毎日ある小さな思い出を日記にしましょう！',
       title: 'Small-diary',
       reverse: true,
       separator: '|',  
-      description: '毎日ある小さな思い出を日記にしましょう！',
+      description: '日記が苦手な人でも大丈夫。あなたが書くのは一言だけ、今日の小さな思い出を一言にするだけ。',
       keywords: 'small-diary',   
       canonical: request.original_url,  
       noindex: ! Rails.env.production?,
-      
+      icon: [
+        { href: image_url('/assets/logo.png') },
+        { href: image_url('/assets/logo.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' },
+      ],
       og: {
         site_name: 'Small-diary',
         title: 'Small-diary',
         description: '毎日ある小さな思い出を日記にしましょう！', 
         type: 'website',
         url: request.original_url,
-        
+        image: image_url('/assets/logo.png'),
         locale: 'ja_JP',
       },
     }
-  end
   end
 end
