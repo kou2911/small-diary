02995d01cdf4cb673c42629003ec0f1cddabe386
kou2911 <pnonkiti@icloud.com>
2022-01-30 21:58:40 +0900

    ogp修正②

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,18 +10,19 @@ module ApplicationHelper
       canonical: request.original_url,  
       noindex: ! Rails.env.production?,
       icon: [
-        { href: image_url('/assets/logo.png') },
-        { href: image_url('/assets/logo.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' },
+        { image: image_url('images/logo.png') },
+        { image: image_url('images/logo.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' },
       ],
       og: {
         site_name: 'Small-diary',
         title: 'Small-diary',
-        description: '毎日ある小さな思い出を日記にしましょう！', 
+        description: '日記が苦手な人でも大丈夫。あなたが書くのは一言だけ、今日の小さな思い出を一言にするだけ。', 
         type: 'website',
         url: request.original_url,
-        image: image_url('/assets/logo.png'),
+        image: image_url('images/logo.png'),
         locale: 'ja_JP',
       },
     }
   end
 end
+
